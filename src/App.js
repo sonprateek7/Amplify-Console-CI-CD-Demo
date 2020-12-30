62647ac811c2fc344a0d0adddcc2a2347d60b847
sonprateek7 <sonprateek7@gmail.com>
2020-12-30 16:07:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://gogoanime.so/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Watch anime
         </a>
       </header>
     </div>
